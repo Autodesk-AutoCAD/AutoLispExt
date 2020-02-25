557e56f4f5cc6129029e1e11be89c2c8c0ba9c05
Hualin Wu <hualin.wu@autodesk.com>
2020-02-24 23:32:34 -0800

    fix the shell injection security issue reported by CI (#78)
    In this submission I correct the os.system() to subprocess.call() to avoid the shell injection security issue, which is reported by CI.
    And I also format the PY file to install the python extension in vscode.

--- pack.py
+++ pack.py
@@ -1,55 +1,63 @@
 import os
-import os.path 
-import shutil, errno
+import subprocess
+import os.path
+import shutil
+import errno
+import shlex
 import platform
 
 # init
+
+
 def init():
-	print("===============================================")
-	print("             try to install gulp-cli globally")
-	os.system("npm install --global gulp-cli")
+    print("===============================================")
+    print("             try to install gulp-cli globally")
+    os.system("npm install --global gulp-cli")
 
-	os.system("npm install --unsafe-perm")
+    os.system("npm install --unsafe-perm")
 
-	print("===============================================")
-	print("             complete npm install")
-	print("===============================================")
-	print("\n\n")
+    print("===============================================")
+    print("             complete npm install")
+    print("===============================================")
+    print("\n\n")
 
-    # "build" means to generate i18n content
-	os.system("gulp build")
+# "build" means to generate i18n content
+    os.system("gulp build")
 
-	print("===============================================")
-	print("          complete gulp build")
-	print("===============================================")
-	print("\n\n")
+    print("===============================================")
+    print("          complete gulp build")
+    print("===============================================")
+    print("\n\n")
 
-	src = os.path.join(os.path.curdir, 'utils', 'acadProcessFinder', 'bin', 'acadProcessFinder.exe')
-	dst = os.path.join(os.path.curdir, 'out', 'process')
-	try:
-		shutil.copy(src, dst)
-		print("===============================================")
-		print("          copied acadProcessFinder.exe")
-		print("===============================================")
-		print("\n\n")
-	except IOError, e:
-		print "Unable to copy file. %s" % e
+    src = os.path.join(os.path.curdir, 'utils',
+                       'acadProcessFinder', 'bin', 'acadProcessFinder.exe')
+    dst = os.path.join(os.path.curdir, 'out', 'process')
+    try:
+        shutil.copy(src, dst)
+        print("===============================================")
+        print("          copied acadProcessFinder.exe")
+        print("===============================================")
+        print("\n\n")
+    except IOError, e:
+        print "Unable to copy file. %s" % e
+
 
 def makepackage_vsix():
-	output_opt = " -o " + os.path.join(os.path.curdir, 'autolispext.vsix')
-	os.system("gulp package" + output_opt)
+    vsce = os.path.join(os.path.curdir, 'node_modules', '.bin', 'vsce.cmd')
+    outputpath = os.path.join(os.path.curdir, 'autolispext.vsix')
+    subprocess.call([vsce, 'package', '-o', outputpath], shell=False)
 
 
 if __name__ == "__main__":
-	init()
+    init()
 
-	print("===============================================")
-	print("      generate vsix package start")
-	print("===============================================")
-	print("\n\n")
-	makepackage_vsix()
-	print("===============================================")
-	print("            congratulations!!")
-	print("      generate vsix package completed!!")
-	print("===============================================")
-	print("\n\n")
+    print("===============================================")
+    print("      generate vsix package start")
+    print("===============================================")
+    print("\n\n")
+    makepackage_vsix()
+    print("===============================================")
+    print("            congratulations!!")
+    print("      generate vsix package completed!!")
+    print("===============================================")
+    print("\n\n")
