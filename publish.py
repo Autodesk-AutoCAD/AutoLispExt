99d1bfc177f51f279fbf5116b42d238c3fd8d535
Sen Lin <Sen.Lin@autodesk.com>
2020-03-10 11:02:49 +0800

    [ACD-8867] Publish autolispext.vsix with Jenkins (#91)
    [ACD-8867] Publish autolispext.vsix with Jenkins (#91)

--- publish.py
+++ publish.py
@@ -1,12 +1,34 @@
 import os
 import os.path 
 import shutil, errno
+import platform
+import sys
 
+def download_artifactory_vsix():
+    if len(sys.argv) < 2:
+        sys.exit(2)
 
+    vsixUri = sys.argv[2]
+
+    print("downloading " + vsixUri);
+
+    os.system("curl -k -O " + vsixUri) # nosec
+
+    fileSize = os.path.getsize('autolispext.vsix')
+
+    print ("executed; file size: " + str(fileSize))
+
+    if fileSize < 1024:
+        print ("ERROR: file size of autolispext.vsix is too small; the downloading failed.")
+        sys.exit(3)
 
 if __name__ == "__main__":
+    argLen = len(sys.argv)
 
-	print("===============================================")
-	print("      publishing to market...")
-	print("===============================================")
-	print("\n\n")
+    if argLen < 1:
+        sys.exit(1)
+
+    print("argv[1] = " + sys.argv[1])
+
+    if sys.argv[1] == "down":
+        download_artifactory_vsix()
