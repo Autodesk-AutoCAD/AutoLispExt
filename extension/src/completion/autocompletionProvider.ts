d0703a19a83acf52b3cc155ede1e7c659e0a4632
Hualin Wu <hualin.wu@autodesk.com>
2020-03-16 10:31:02 +0800

    fix underline and mid-line command case for ACD-9011 (#95)
    In this submission I have done the following things:
     1. fix the fix underline and mid-line matching command case for ACD-9011
     2. refactor the codes 
    
    Please note that it looks like the VSCODE completion only works for the "startwith" case, it can not work for the "endwith" case, and it can not work "include" case:
      **startwith**: the input word is starting with the completion items
      **endwith**: the input word is ending with the completion items
      **Include**: the input word is in the middle of word for the completion items

--- extension/src/completion/autocompletionProvider.ts
+++ extension/src/completion/autocompletionProvider.ts
@@ -72,18 +72,128 @@ export function readAllBultinFunctions() {
     readDataFileByDelimiter("../../extension/data/allsysvars.txt", " ", (item) => { allSysvars.push(item) });
 }
 
-function getCompletionCandidates(allCandiates: string[], word: string, userInputIsUpper: boolean): Array<vscode.CompletionItem> {
-    let allSuggestions: Array<vscode.CompletionItem> = [];
+function getCmdAndVarsCompletionCandidates(allCandiates: string[], word: string, userInputIsUpper: boolean): Array<vscode.CompletionItem> {
+    var hasUnderline = false;
+    if (word[0] == "_") {
+        hasUnderline = true;
+        word = word.substring(1);
+    }
+
+    var hasDash = false;
+    if (word[0] == "-") {
+        hasDash = true;
+    }
+
+    let suggestions: Array<vscode.CompletionItem> = [];
     allCandiates.forEach((item) => {
         var candidate = item;
         if (userInputIsUpper)
             candidate = item.toUpperCase();
-        if (candidate.startsWith(word) || candidate.endsWith(word)) {
-            const completion = new vscode.CompletionItem(candidate);
-            allSuggestions.push(completion);
+        else
+            candidate = item.toLowerCase();
+
+        if (candidate.startsWith(word)) {
+            var label = candidate;
+
+            // The _ symbol has special mean in AutoCAD commands, so we add the prefix if it matches the command name
+            if (hasUnderline)
+                label = "_" + label;
+
+            const completion = new vscode.CompletionItem(label);
+
+            // to work around the middle dash case issue in vscode, when insert it ignores the dash
+            if (hasDash)
+                completion.insertText = label.substring(1);
+
+            suggestions.push(completion);
         }
     });
 
+    return suggestions;
+}
+
+function getCompletionCandidates(allCandiates: string[], word: string, userInputIsUpper: boolean): Array<vscode.CompletionItem> {
+    let suggestions: Array<vscode.CompletionItem> = [];
+    allCandiates.forEach((item) => {
+        var candidate = item;
+        if (userInputIsUpper)
+            candidate = item.toUpperCase();
+        else
+            candidate = item.toLowerCase();
+
+        if (candidate.startsWith(word)) {
+            var label = candidate;
+            const completion = new vscode.CompletionItem(label);
+            suggestions.push(completion);
+        }
+    });
+
+    return suggestions;
+}
+
+function getMatchingWord(document: vscode.TextDocument, position: vscode.Position): [string, boolean]{
+    let linetext = document.lineAt(position).text;
+
+    let word = document.getText(document.getWordRangeAtPosition(position));
+    let wordSep = " &#^()[]|;'\".";
+
+    // Autolisp has special word range rules and now VScode has some issues to check the "word" range, 
+    // so it needs this logic to check the REAL word range
+    let pos = position.character;
+    pos -= 2;
+    let length = 1;
+    let hasSetLen = false;
+    for (; pos >= 0; pos--) {
+        let ch = linetext.charAt(pos);
+        if (wordSep.includes(ch)) {
+            if (!hasSetLen)
+                length = word.length;
+            word = linetext.substr(pos + 1, length);
+            break;
+        }
+        length++;
+        hasSetLen = true;
+    }
+
+    var isupper = () => {
+        var lastCh = word.slice(-1);
+        var upper = lastCh.toUpperCase();
+        if (upper != lastCh.toLowerCase() && upper == lastCh)
+            return true;
+        return false;
+    }
+    var inputIsUpper = isupper();
+    if (inputIsUpper)
+        word = word.toUpperCase();
+    else word = word.toLowerCase();
+
+    return [word, inputIsUpper];
+}
+
+function getLispAndDclCompletions(document: vscode.TextDocument, word:string, isupper:boolean): vscode.CompletionItem[]
+{
+    let currentLSPDoc = document.fileName;
+    let ext = currentLSPDoc.substring(currentLSPDoc.length - 4, currentLSPDoc.length).toUpperCase();
+    let candidatesItems = internalLispFuncs;
+    if (ext === ".DCL") {
+        candidatesItems = internalDclKeys;
+    }
+    let allSuggestions: Array<vscode.CompletionItem> = [];
+    allSuggestions = getCompletionCandidates(candidatesItems, word, isupper);
+
+    if (os.platform() === "win32") {
+        return allSuggestions;
+    }
+    else {
+        return allSuggestions.filter(function(suggestion) {
+            for (var prefix of winOnlyListFuncPrefix) {
+                if (suggestion.label.startsWith(prefix)) {
+                    return false;
+                }
+            }
+            return true;
+        });
+    }
     return allSuggestions;
 }
 
@@ -93,74 +203,27 @@ export function registerAutoCompletionProviders() {
         provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
             try {
-
-                let currentLSPDoc = document.fileName;
-                let ext = currentLSPDoc.substring(currentLSPDoc.length - 4, currentLSPDoc.length).toUpperCase();
-                let candidatesItems = internalLispFuncs;
-                if (ext === ".DCL") {
-                    candidatesItems = internalDclKeys;
-                }
-
-                // If it is in comments, it doesn't need to provide lisp autocomplete
                 let linetext = document.lineAt(position).text;
                 if (linetext.startsWith(";") || linetext.startsWith(";;")
-                    || linetext.startsWith("#|") || linetext.startsWith("|#")) {
+                    || linetext.startsWith("#|")) {
                     return [];
                 }
 
-                let word = document.getText(document.getWordRangeAtPosition(position));
-                let wordSep = " &#^()[]|;'\".";
-
-                // Autolisp has special word range rules and now VScode has some issues to check the "word", 
-                // so it needs this logic to check the REAL word range
-                let pos = position.character;
-                pos -= 2;
-                let length = 0;
-                for (; pos >= 0; pos--) {
-                    let ch = linetext.charAt(pos);
-                    if (wordSep.includes(ch)) {
-                        if (length == 0)
-                            length = word.length;
-                        word = linetext.substr(pos + 1, length);
-                        break;
-                    }
-                    length++;
-                }
-                if (word.length == 0)
+                let [inputword, userInputIsUpper] = getMatchingWord(document, position);
+                if (inputword.length == 0)
                     return [];
-                var isupper = () => {
-                    if (word === word.toLocaleUpperCase())
-                        return true;
-                    return false;
-                }
-                var userInputIsUpper = isupper();
 
-                let allSuggestions: Array<vscode.CompletionItem> = [];
 
                 var isInDoubleQuote = isCursorInDoubleQuoteExpr(document, position);
                 if (isInDoubleQuote) {
-                    allSuggestions = allSuggestions.concat(getCompletionCandidates(allArxCmds, word, userInputIsUpper));
+                    var cmds = getCmdAndVarsCompletionCandidates(allArxCmds, inputword, userInputIsUpper);
 
-                    allSuggestions = allSuggestions.concat(getCompletionCandidates(allSysvars, word, userInputIsUpper));
-
-                    return allSuggestions;
+                    var vars = getCmdAndVarsCompletionCandidates(allSysvars, inputword, userInputIsUpper);
+                    var res = cmds.concat(vars);
+                    return res;
                 }
 
-                allSuggestions = allSuggestions.concat(getCompletionCandidates(candidatesItems, word, userInputIsUpper));
-
-                if (os.platform() === "win32") {
-                    return allSuggestions;
-                }
-                else {
-                    return allSuggestions.filter(function(suggestion) {
-                        for (var prefix of winOnlyListFuncPrefix) {
-                            if (suggestion.label.startsWith(prefix)) {
-                                return false;
-                            }
-                        }
-                        return true;
-                    });
-                }
+                return getLispAndDclCompletions(document, inputword, userInputIsUpper);
             }
             catch (err) {
                 return [];
