1d20257cc5bf2edad1e782f44ddae0d88453c826
Sen Lin <Sen.Lin@autodesk.com>
2020-05-08 10:21:06 +0800

    [ACD-11067] Lpp: as an installer I want to make one vsix for all platforms (#124)
    [ACD-11067] Lpp: as an installer I want to make one vsix for all platforms (#124)

--- pack.py
+++ pack.py
@@ -29,18 +29,44 @@ def init():
     print("===============================================")
     print("\n\n")
 
+    copyAcadProcFinder()
+
+    copyRipGrep()
+
+def copyRipGrep():
+    #copy exe of vscode-ripgrep into bin folder for both Windows and Mac
+    srcWin = os.path.join(os.path.curdir, 'utils', 'vscode-ripgrep', 'ripgrep-v11.0.1-2-x86_64-pc-windows-msvc', 'rg.exe')
+    srcOSX = os.path.join(os.path.curdir, 'utils', 'vscode-ripgrep', 'ripgrep-v11.0.1-2-x86_64-apple-darwin', 'rg')
+    dst = os.path.join(os.path.curdir, 'node_modules', 'vscode-ripgrep', 'bin')
+
+    removeFile(dst, 'rg.exe')
+    removeFile(dst, 'rg')
+    copyFile(srcWin, dst, 'copied rg.exe for Windows')
+    copyFile(srcOSX, dst, 'copied rg for OS X')
+
+
+def copyAcadProcFinder():
     src = os.path.join(os.path.curdir, 'utils',
                        'acadProcessFinder', 'bin', 'acadProcessFinder.exe')
     dst = os.path.join(os.path.curdir, 'out', 'process')
+
+    copyFile(src, dst, 'copied acadProcessFinder.exe')
+
+
+def removeFile(dir, filename):
+    filepath = os.path.join(dir, filename)
+    if os.path.exists(filepath):
+        os.remove(filepath)
+
+def copyFile(src, dst, description):
     try:
         shutil.copy(src, dst)
         print("===============================================")
-        print("          copied acadProcessFinder.exe")
+        print("          " + description)
         print("===============================================")
         print("\n\n")
     except IOError, e:
         print "Unable to copy file. %s" % e
-
 
 def makepackage_vsix():
     print("===============================================")
---
+++ utils/vscode-ripgrep/readme.txt
@@ -0,0 +1,5 @@
+They're downloaded from the following URL:
+
+https://github.com/microsoft/ripgrep-prebuilt/releases
+
+Current version: v11.0.1-2
