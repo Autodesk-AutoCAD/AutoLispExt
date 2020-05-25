56ba1e95e43e0276f2381c0cbac3549691204cd2
Zhongyu Fang <zhongyu.fang@autodesk.com>
2020-05-25 17:05:41 +0800

    don't need to refresh if no project is opened (#166)
    Check if there's project opened before reopen it.

--- extension/src/project/refreshProject.ts
+++ extension/src/project/refreshProject.ts
@@ -3,6 +3,9 @@ import { ProjectTreeProvider } from './projectTree';
 import * as vscode from 'vscode';
 
 export function RefreshProject(){
+	if (!ProjectTreeProvider.hasProjectOpened()) {
+		return;
+	}
 	let prjFilePath = ProjectTreeProvider.instance().projectNode.projectFilePath;
 	let prjNode = OpenProjectFile(vscode.Uri.file(prjFilePath));
 	ProjectTreeProvider.instance().updateData(prjNode);
