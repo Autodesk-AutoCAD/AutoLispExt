11fed32da9ca77b31e3200ec7f29724f3131ff72
Hualin Wu <hualin.wu@autodesk.com>
2020-06-10 00:09:18 -0700

    add prefix "format" for all the 4 formatting settings (#190)
    add prefix format for all the 4 formatting settings
    
    ### Abstraction
    In this commit I add the prefxi for all the formatting settings to avoid the messy layout between the Debug and Format settings.
    
    Before the changes, all the "debug" setting is in the middle of the formating settings, and the two types setting are not STYLE consistent:
    ![image](https://media.git.autodesk.com/user/4246/files/47362f00-aa8f-11ea-8f37-c85a97af30d3)
    
    Now the settings looks like this:
    ![image](https://media.git.autodesk.com/user/4246/files/61224280-aa8c-11ea-809b-1ef6e204018d)
    
    ### Note to TD
    Please test that the format setting still take effective after changing it.
    https://jira.autodesk.com/browse/ACD-12267

--- extension/src/format/fmtconfig.ts
+++ extension/src/format/fmtconfig.ts
@@ -2,17 +2,17 @@ import { workspace } from 'vscode';
 
 
 export function closeParenStyle(): 'Same line' | 'New line with outer identation' {
-    return workspace.getConfiguration('autolispext').get('CloseParenthesisStyle') || 'New line with outer identation';
+    return workspace.getConfiguration('autolispext').get('format.CloseParenthesisStyle') || 'New line with outer identation';
 }
 
 export function maximumLineChars(): number {
-    return workspace.getConfiguration('autolispext').get('MaxLineChars') || 85;
+    return workspace.getConfiguration('autolispext').get('format.MaxLineChars') || 85;
 }
 
 export function longListFormatStyle(): 'Single Column' | 'Fill to Margin' {
-    return workspace.getConfiguration('autolispext').get('LongListFormatStyle') || 'Fill to Margin';
+    return workspace.getConfiguration('autolispext').get('format.LongListFormatStyle') || 'Fill to Margin';
 }
 
 export function indentSpaces(): number {
-    return workspace.getConfiguration('autolispext').get('NarrowStyleIndent') || 2;
+    return workspace.getConfiguration('autolispext').get('format.NarrowStyleIndent') || 2;
 }
