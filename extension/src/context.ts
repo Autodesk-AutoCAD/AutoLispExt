3d9d4ad01abac648693586abc80262584a9c3e41
Joshua Howard <51800232+JD-Howard@users.noreply.github.com>
2020-10-25 19:11:05 -0700

    Context & Document Managers (#32)
    * Context & Document Managers
    
    * incorporated comments & added documentation

---
+++ extension/src/context.ts
@@ -0,0 +1,41 @@
+import * as vscode from "vscode";
+import * as nls from "vscode-nls";
+import * as Resources from "./resources";
+import { Disposable } from 'vscode-languageclient';
+import { DocumentManager } from './documents';
+
+
+export class ContextManager{
+	private _ctx: vscode.ExtensionContext;
+	private _docManager: DocumentManager;
+	
+	localize = nls.config({ messageFormat: nls.MessageFormat.file })();	
+	
+	get Context(): vscode.ExtensionContext { 
+		return this._ctx; 
+	}
+	get Documents(): DocumentManager { 
+		return this._docManager; 
+	}
+	get Selectors(): typeof DocumentManager.Selectors { 
+		return DocumentManager.Selectors; 
+	}
+	get AllSelectors(): string[] { 
+		return [DocumentManager.Selectors.lsp, DocumentManager.Selectors.dcl, DocumentManager.Selectors.prj]; 
+	}
+	get Subscriptions(): Disposable[] { 
+		return this._ctx ? this._ctx.subscriptions : null; 
+	}
+	get ExtPath(): string { 
+		return this._ctx ? this._ctx.extensionPath : ""; 
+	}
+	get Data() { 
+		return Resources; 
+	}
+
+	
+	initialize(context: vscode.ExtensionContext): void {
+		this._ctx = context;
+		this._docManager = new DocumentManager();
+	}
+}
