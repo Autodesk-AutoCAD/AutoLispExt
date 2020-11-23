2d9bbd1c678c760631cda04175ec62de2f00c57c
JD-Howard <howardjosh@hotmail.com>
2020-11-23 05:51:01 -0700

    AtomsForest Replacement & Tests

--- extension/src/format/parser.ts
+++ extension/src/format/parser.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import { ListReader, CursorPosition } from "./listreader";
-import { Sexpression } from "./sexpression";
+import { LispAtom, Sexpression } from "./sexpression";
 
 class LeftParentItem {
     public location: Number;
@@ -181,5 +181,150 @@ export class LispParser {
             let lispLists = reader.tokenize();
             this.atomsForest.push(lispLists);
         }
+    }
+}
+
+
+export namespace LispParser {
+    
+    enum ParseState {
+        UNKNOWN = 0,
+        STRING = 1,
+        COMMENT = 2
+    }
+
+    interface CountTracker {
+        idx: number;
+        line: number;
+        column: number;
+        linefeed: string;
+    }
+
+    export function getDocumentSexpression(data: string|vscode.TextDocument, tracker?: CountTracker): Sexpression {
+        let documentText = '';
+        if (typeof(data) === 'string') {
+            documentText = data;
+        } else {
+            documentText = data.getText();
+        }
+        if (!tracker){
+            tracker = {
+                idx: 0, line: 0, column: 0, 
+                linefeed: typeof(data) === 'string' ? (data.indexOf('\r\n') >= 0 ? '\r\n' : '\n') : LispParser.getEOL(data)
+            };
+        }
+        
+        const result = new Sexpression();
+        result.line = tracker.line;
+        result.column = tracker.column;
+        result.linefeed = tracker.linefeed;
+        
+        let isAuthorized = true;
+        let state = ParseState.UNKNOWN;
+        let grpStart: vscode.Position = null;
+        let temp = '';
+        
+        while (tracker.idx < documentText.length && isAuthorized) {
+            //const prev = documentText[tracker.idx - 1];
+            const curr = documentText[tracker.idx];
+            const next = documentText[tracker.idx + 1];
+            let handled = false;
+            switch (state) {
+                case ParseState.UNKNOWN:
+                    if (grpStart === null && curr === '\'' && next === '(') {
+                        result.atoms.push(new LispAtom(tracker.line, tracker.column, curr));
+                    } else if (grpStart === null && curr === '(') {
+                        grpStart = new vscode.Position(tracker.line, tracker.column);
+                        result.atoms.push(new LispAtom(grpStart.line, grpStart.character, curr));
+                    } else if (curr === ')') {
+                        if (temp.length > 0) {                            
+                            result.atoms.push(new LispAtom(grpStart.line, grpStart.character, temp));
+                        }
+                        temp = '';
+                        result.atoms.push(new LispAtom(tracker.line, tracker.column, curr));
+                        grpStart = null;
+                        isAuthorized = false;
+                    } else if (curr === '(' || curr === '\'' && next === '(') {
+                        if (temp.length > 0) {
+                            result.atoms.push(new LispAtom(grpStart.line, grpStart.character, temp));                            
+                        }
+                        temp = '';
+                        result.atoms.push(getDocumentSexpression(documentText, tracker));
+                        handled = true;
+                    } else if (curr === ';') {
+                        if (temp.length > 0) {
+                            result.atoms.push(new LispAtom(grpStart.line, grpStart.character, temp));
+                        }
+                        temp = ';';
+                        grpStart = new vscode.Position(tracker.line, tracker.column);
+                        state = ParseState.COMMENT;
+                    } else if (curr === '"') {
+                        if (temp.length > 0) {
+                            result.atoms.push(new LispAtom(grpStart.line, grpStart.character, temp));
+                        }
+                        temp = '"';
+                        grpStart = new vscode.Position(tracker.line, tracker.column);
+                        state = ParseState.STRING;
+                    } else if (/\s/.test(curr)) {
+                        if (temp.length > 0) {
+                            result.atoms.push(new LispAtom(grpStart.line, grpStart.character, temp));
+                        }
+                        if (curr === '\n'){
+                            tracker.line++;
+                            tracker.column = -1;
+                        }
+                        temp = '';
+                    } else { // This is some other kind of readable character, so it can start a group pointer
+                        if (temp === '') { 
+                            grpStart = new vscode.Position(tracker.line, tracker.column);
+                        }
+                        temp += curr;
+                    }
+                    break;
+                case ParseState.STRING:
+                    temp += curr;
+                    // these 2 endswith tests are hard to understand, but they were vetted on a previous C# lisp parser to detect escaped double quotes
+                    if (curr === '"' && (temp.endsWith("\\\\\"") || !temp.endsWith("\\\""))) {    
+                        result.atoms.push(new LispAtom(grpStart.line, grpStart.character, temp));
+                        state = ParseState.UNKNOWN;
+                        temp = '';
+                    } 
+                    if (curr === '\n'){
+                        tracker.line++;
+                        tracker.column = -1;
+                    }
+                    break;
+                case ParseState.COMMENT:                    
+                    if (temp[1] === '|') {
+                        temp += curr;
+                        if (temp.endsWith('|;')) {
+                            result.atoms.push(new LispAtom(grpStart.line, grpStart.character, temp));
+                            state = ParseState.UNKNOWN;
+                            temp = '';
+                        }
+                        if (curr === '\n'){
+                            tracker.line++;
+                            tracker.column = -1;
+                        }
+                    } else {
+                        if (curr === '\r' || curr === '\n') {
+                            result.atoms.push(new LispAtom(grpStart.line, grpStart.character, temp));
+                            state = ParseState.UNKNOWN;
+                            temp = '';
+                        } else {
+                            temp += curr;
+                        }
+                    }
+                    break;
+            }
+            if (handled === false) {
+                tracker.idx++;                
+                tracker.column++;
+            }
+        }
+        if (temp.length > 0) {
+            result.atoms.push(new LispAtom(grpStart.line, grpStart.character, temp));
+        }
+        return result;
     }
 }
---
+++ extension/src/test/suite/DocumentExpression.test.ts
@@ -0,0 +1,58 @@
+import * as path from 'path';
+import { LispParser } from '../../format/parser';
+import { Sexpression } from '../../format/sexpression';
+import { ReadonlyDocument } from '../../project/readOnlyDocument';
+
+var assert = require('chai').assert;
+let project_path = path.join(__dirname + "\\..\\..\\..\\test_case\\pdfMarkups.lsp");
+
+
+suite("LispParser.DocumentExpression Tests", function () {	
+	test("Original atomsForest vs new version", function () {		
+		try {
+			const doc = ReadonlyDocument.open(project_path); 			
+			const dex = LispParser.getDocumentSexpression(doc);
+			const v1Start = Date.now();
+			const v1items = doc.atomsForest.filter(x => x instanceof Sexpression);
+			const v1Stop = Date.now();
+			const v2items = dex.atoms.filter(x => x instanceof Sexpression);
+			const v2Stop = Date.now();
+			const v1Diff = v1Stop - v1Start;
+			const v2Diff = v2Stop - v1Stop;
+			assert.isBelow(v2Diff, v1Diff);
+			assert.equal(v2items.length, v1items.length);
+			
+		}
+		catch (err) {
+			assert.fail("Each version returned a different number of Sexpressions");
+		}
+	});
+
+
+	test("String Source: Test Unix EOLs", function () {
+		try { 
+			const val = '(defun C:DoStuff (/ pt)\n\t(setq pt (getpoint))\n\t(command ".point" pt)\n\t)';
+			const dex = LispParser.getDocumentSexpression(val);
+			assert.equal(dex.atoms.length, 7);
+			assert.equal(dex.linefeed, '\n');
+		}
+		catch (err) {
+			assert.fail("Incorrect parse quantity or EOL value");
+		}
+	});
+
+	test("String Source: Test Windows EOLs", function () {
+		try { 
+			const val = '(defun C:DoStuff (/ pt)\r\n\t(setq pt (getpoint))\r\n\t(command ".point" pt)\r\n\t)';
+			const dex = LispParser.getDocumentSexpression(val);
+			assert.equal(dex.atoms.length, 7);
+			assert.equal(dex.linefeed, '\r\n');
+		}
+		catch (err) {
+			assert.fail("Incorrect parse quantity or EOL value");
+		}
+	});
+
+});
+
+
