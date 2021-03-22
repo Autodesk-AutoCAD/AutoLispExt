368afb9820bff5e7bb966b1f2cd0edb2aff767e5
Sen Lin <Sen.Lin@autodesk.com>
2021-03-22 00:17:31 -0700

    initial version

--- extension/src/test/suite/index.ts
+++ extension/src/test/suite/index.ts
@@ -2,7 +2,21 @@ import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
-export function run(): Promise<void> {
+const NYC = require('nyc');
+import * as baseConfig from "@istanbuljs/nyc-config-typescript";
+import 'ts-node/register';
+import 'source-map-support/register';
+
+// Linux: prevent a weird NPE when mocha on Linux requires the window size from the TTY
+// Since we are not running in a tty environment, we just implementt the method statically
+const tty = require('tty');
+if (!tty.getWindowSize) {
+    tty.getWindowSize = (): number[] => {
+        return [80, 75];
+    };
+}
+
+export async function run(): Promise<void> {
 	// Create the mocha test
 	const mocha = new Mocha({
 		ui: 'tdd'
@@ -11,28 +25,54 @@ export function run(): Promise<void> {
 
 	const testsRoot = path.resolve(__dirname, '..');
 
-	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
-
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				e(err);
-			}
-		});
+	// Setup coverage pre-test, including post-test hook to report
+	const nyc = new NYC({
+		...baseConfig,
+		cwd: path.join(__dirname, '..', '..', '..'),
+		reporter: ['text-summary', 'html'],
+		all: true,
+		silent: false,
+		instrument: true,
+		hookRequire: true,
+		hookRunInContext: true,
+		hookRunInThisContext: true,
+		include: ["out/**/*.js"],
+		exclude: ["out/test/**"],
 	});
+	await nyc.wrap();
+
+	// Debug which files will be included/excluded
+	console.log('Glob verification', await nyc.exclude.glob(nyc.cwd));
+
+	// Check the modules already loaded and warn in case of race condition
+	// (ideally, at this point the require cache should only contain one file - this module)
+	const myFilesRegex = /AutoLispExt\/out/;
+	const filterFn = myFilesRegex.test.bind(myFilesRegex);
+	if (Object.keys(require.cache).filter(filterFn).length > 1) {
+		console.warn('NYC initialized after modules were loaded', Object.keys(require.cache).filter(filterFn));
+	}
+
+	await nyc.createTempDirectory();
+
+	// Add all files to the test suite
+	const files = glob.sync('**/*.test.js', { cwd: testsRoot });
+	files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+	const failures: number = await new Promise(resolve => mocha.run(resolve));
+	await nyc.writeCoverageFile();
+
+	// Capture text-summary reporter's output and log it in console
+	console.log(await captureStdout(nyc.report.bind(nyc)));
+
+	if (failures > 0) {
+		throw new Error(`${failures} tests failed.`);
+	}
+
+	async function captureStdout(fn) {
+		let w = process.stdout.write, buffer = '';
+		process.stdout.write = (s) => { buffer = buffer + s; return true; };
+		await fn();
+		process.stdout.write = w;
+		return buffer;
+	}
 }
