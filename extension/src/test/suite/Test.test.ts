476a680a8fa4892ad8b0a3856eab71eceef62ce7
Nigma Liu <Nigma.Liu@autodesk.com>
2021-03-23 15:33:37 +0800

    Add more test

--- extension/src/test/suite/Test.test.ts
+++ extension/src/test/suite/Test.test.ts
@@ -5,7 +5,7 @@ import { Position, Range } from 'vscode';
 import { LispParser } from '../../format/parser';
 import { Sexpression, LispContainer } from '../../format/sexpression';
 import { ReadonlyDocument } from '../../project/readOnlyDocument';
-import fs from 'fs';
+import { readFile,readFileSync } from 'fs';
 
 // var assert = require('chai').assert;
 let assert = chai.assert;
@@ -15,14 +15,15 @@ console.log('lispFileTest is ' + lispFileTest);
 suite("LispParser.DocumentContainer Tests", function () {	
 	test("empty test to run", function () {	
 		try {
-			// const doc = fs.readFileSync(lispFileTest,'utf8')
+			let doc = readFileSync(lispFileTest,'utf8')
+			console.log("Asynchronous read: " + doc.toString());
 
-			fs.readFile(lispFileTest, function (err, data) {
-				if (err) {
-					return console.error(err);
-				}
-				console.log("Asynchronous read: " + data.toString());
-			});
+			// readFile(lispFileTest, function (err, data) {
+			// 	if (err) {
+			// 		return console.error(err);
+			// 	}
+			// 	console.log("Asynchronous read: " + data.toString());
+			// });
 		}
 		catch (err) {
 			assert.fail("Test Failed!");
