9348f8f5bc61376a2914b20d5a938f8f87bce431
Nigma Liu <Nigma.Liu@autodesk.com>
2021-03-23 18:36:26 +0800

    test

--- extension/src/test/suite/Test.test.ts
+++ extension/src/test/suite/Test.test.ts
@@ -17,14 +17,30 @@ suite("LispParser.DocumentContainer Tests", function () {
 		try {
 			// let doc = readFileSync(lispFileTest,'utf8')
 			// console.log("Asynchronous read: " + doc.toString());
+			
+			const doc = ReadonlyDocument.open(lispFileTest); 
+			console.log('file is opened');
 			const v1Start = Date.now();
-			console.log('Date.now start is ' + v1Start);
-			readFile(lispFileTest, function (err, data) {
-				if (err) {
-					return console.error(err);
-				}
-				console.log("Asynchronous read sucess: ");
-			});
+			const parser = new LispParser(doc);
+			parser.tokenizeString(doc.getText(), 0);
+			const v1items = parser.atomsForest.filter(x => x instanceof Sexpression);
+			const v1Stop = Date.now();
+			const dex = LispParser.getDocumentContainer(doc.fileContent);			
+			const v2items = dex.atoms.filter(x => x instanceof LispContainer);
+			const v2Stop = Date.now();			
+			const v1Diff = v1Stop - v1Start;
+			const v2Diff = v2Stop - v1Stop;
+			console.log(`\t\tNewParser Processing Time: ${v2Diff}ms`);
+			console.log(`\t\tOldParser Processing Time: ${v1Diff}ms`);
+			assert.isTrue(v2Diff <= v1Diff || v1Diff - v2Diff <= 1);
+			assert.equal(v2items.length, v1items.length);
+
+			// readFile(lispFileTest, function (err, data) {
+			// 	if (err) {
+			// 		return console.error(err);
+			// 	}
+				
+			// });
 		}
 		catch (err) {
 			assert.fail("Test Failed!");
