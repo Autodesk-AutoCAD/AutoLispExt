60b319a4359a15bc16bebca87bc43408ecf5a4d0
Nigma Liu <Nigma.Liu@autodesk.com>
2021-03-23 15:04:09 +0800

    Modify the DcoumentContainer test

--- extension/src/test/suite/DocumentContainer.test.ts
+++ extension/src/test/suite/DocumentContainer.test.ts
@@ -1,10 +1,13 @@
 import * as path from 'path';
+import * as chai from 'chai';
+
 import { Position, Range } from 'vscode';
 import { LispParser } from '../../format/parser';
 import { Sexpression, LispContainer } from '../../format/sexpression';
 import { ReadonlyDocument } from '../../project/readOnlyDocument';
 
-var assert = require('chai').assert;
+// var assert = require('chai').assert;
+let assert = chai.assert;
 let lispFileTest = path.join(__dirname + "/../../../test_case/pdfMarkups.lsp");
 console.log('lispFileTest is ' + lispFileTest);
 
