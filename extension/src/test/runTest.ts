8cab9db25cbaf623d036c219726a0db6763de447
Nigma Liu <Nigma.Liu@autodesk.com>
2021-03-26 11:18:40 +0800

    Improvement code coverage to run with argument

--- extension/src/test/runTest.ts
+++ extension/src/test/runTest.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-
+// import * as process from 'process';
 import { runTests } from 'vscode-test';
 
 async function main() {
@@ -7,14 +7,22 @@ async function main() {
 		// The folder containing the Extension Manifest package.json
 		// Passed to `--extensionDevelopmentPath`
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../../../');
-
+		let extensionTestsPath ='';
+		console.log('argv[0] is ' + process.argv[0]);
+		console.log('argv[1] is ' + process.argv[1]);
+		console.log('argv[2] is ' + process.argv[2]);
 		// The path to the extension test script
 		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
-
+		if (process.argv[2] === '--codecoverage') {
+		 	extensionTestsPath = path.resolve(__dirname, './suite/codeCoverage');
+		} else {
+			extensionTestsPath = path.resolve(__dirname, './suite/index');
+		}
 		// Download VS Code, unzip it and run the integration test
+		console.log('extensionTestsPath is ' + extensionTestsPath);
 		await runTests({ extensionDevelopmentPath, extensionTestsPath });
 	} catch (err) {
+		console.error('err is ' + err);
 		console.error('Failed to run tests');
 		process.exit(1);
 	}
