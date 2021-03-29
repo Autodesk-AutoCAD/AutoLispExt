91ff4fe390e9caf0a685c8e0feb563e1161902ef
Sen Lin <Sen.Lin@autodesk.com>
2021-03-28 18:19:10 -0700

    more test

--- extension/src/test/e2e/helloE2e.ts
+++ extension/src/test/e2e/helloE2e.ts
@@ -1,18 +1,23 @@
 // import the webdriver and the high level browser wrapper
-import { assert } from 'chai';
-import { VSBrowser, WebDriver, Workbench } from 'vscode-extension-tester';
+import { assert, expect } from 'chai';
+import { QuickOpenBox, VSBrowser, WebDriver, Workbench } from 'vscode-extension-tester';
 
 // Create a Mocha suite
 describe('My Test Suite', () => {
   let browser: VSBrowser;
-  let driver: WebDriver
+  let driver: WebDriver;
+  let input: QuickOpenBox;
   
   // initialize the browser and webdriver
   before(async () => {
     browser = VSBrowser.instance;
     driver = browser.driver;
+    input = await new Workbench().openCommandPrompt();
   });
-  
+
+  after(async () => {
+      await input.cancel();
+  });  
   // test whatever we want using webdriver, here we are just checking the page title
 //   it('My Test Case', async () => {
 //     const title = await driver.getTitle();
@@ -21,7 +26,13 @@ describe('My Test Suite', () => {
 
   it('should show debugger selector', async () => {
     const workbench = new Workbench();
-    await workbench.executeCommand('Debug: Start Debugging');
+    await workbench.executeCommand('workbench.action.debug.start');
+    expect(await input.isDisplayed()).true;
+    const picks = await input.getQuickPicks();
+    expect(picks.length).equals(1);
+
+    const pick = await input.findQuickPick('Select Environment');
+    expect(pick).not.undefined;
   });
 
 });
