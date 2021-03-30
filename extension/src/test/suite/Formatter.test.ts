bba04874b13e3d72f1ddd1e2c3690e324613310f
Nigma Liu <Nigma.Liu@autodesk.com>
2021-03-30 08:51:34 +0800

    Add format test

--- extension/src/test/suite/Formatter.test.ts
+++ extension/src/test/suite/Formatter.test.ts
@@ -1,5 +1,5 @@
 import * as chai from 'chai';
-// import * as chaiFiles from 'chai-files';
+import * as chaiFiles from 'chai-files';
 import { Position } from 'vscode';
 import { LispAtom } from '../../format/sexpression';
 import { LispFormatter } from '../../format/formatter';
@@ -10,31 +10,28 @@ import * as path from 'path';
 
 let prettydiff = require("prettydiff");
 console.log('__dirname in test context is :' + __dirname);
-let unformat_file = path.join(__dirname + "/../../../extension/src/test/Source/unFormatted.lsp");
-let formatedfile = path.join(__dirname + "/formatedfile.lsp");
-let formatedBasefile = path.join(__dirname + "/../../../extension/src/test/Base/formatedBasefile.lsp");
-// chai.use(chaiFiles);
+let unformat_file1 = path.join(__dirname + "/../../../extension/src/test/SourceFile/unFormatted1.lsp");
+let formatedOutputFile1 = path.join(__dirname + "/../../../extension/src/test/OutputFIle/formatedOutputFile1.lsp");
+let formatedBasefile1 = path.join(__dirname + "/../../../extension/src/test/Baseline/formatedBasefile1.lsp");
+chai.use(chaiFiles);
 
 let expect = chai.expect;
-// let file = chaiFiles.file;
-// let dir = chaiFiles.dir;
-let output     = "",
-    options    = prettydiff.options;
-options.source = "my code";
-output         = prettydiff();
+let file = chaiFiles.file;
+let dir = chaiFiles.dir;
 
-console.log('output in test context is :' + output);
+
+// console.log('output in test context is :' + output);
 
 suite("LispFormatter Tests", function () {	
 	try {
 		
-		const doc = ReadonlyDocument.open(unformat_file); 						
-		let fmt = LispFormatter.format(doc, null);
-		fs.writeFile(unformat_file, fmt, err => {
-			if (err) {
-			 console.error('Failed to write file: ', err);
-			} else console.log('write file failed');
-		   });
+		
+			// output = prettydiff.api({
+			// 	source: source, 
+			// 	mode: 'diff',
+			// 	diff: target, 
+			// 	lang: 'text',
+			//   });
 		let expectedFormatString =  
 `(defun c:formatTest (/ a b) 
   (setq a 3)
@@ -46,13 +43,50 @@ suite("LispFormatter Tests", function () {
 		test("LispFormatter.format() Test", function () {
 			try {
 				// chai.assert.equal(formatedstring.length,fmt.length);
-				chai.assert.equal(fmt,expectedFormatString);
+				// chai.assert.equal(fmt,expectedFormatString);
+				const doc = ReadonlyDocument.open(unformat_file1); 						
+				let fmt = LispFormatter.format(doc, null);
+				fs.writeFile(formatedOutputFile1, fmt, err => {
+						if (err) {
+							console.error('Failed to write file: ', err);
+						} 
+						// let output     = "",
+						// options    = prettydiff.options;
+						// options.source = formatedOutputFile1;
+						// options.diff = formatedBasefile1;
+						// options.api === "node";
+						// options.source = fs.readFileSync(formatedOutputFile1);
+						// console.log('formatedOutputFile1 content is ' +fs.readFileSync(formatedOutputFile1) );
+						// console.log('formatedBasefile1 content is ' +fs.readFileSync(formatedBasefile1) );
+						// options.diff = fs.readFileSync(formatedBasefile1);
+						// output = prettydiff();
+						// output = prettydiff.api({
+						// 	source: options.source, 
+						// 	mode: 'diff',
+						// 	diff: options.diff, 
+						// 	lang: 'text',
+						//   });
+						// console.log('diff is :' + output[0]);
+						expect(file(formatedOutputFile1)).to.equal(file(formatedBasefile1));
 
+					});
+
+		// }, options = prettydiff.options, lf = (options.crlf === true)
+
+			// prettydiff.api();
+			// output = prettydiff.api({
+			// 	source: options.source, 
+			// 	mode: 'diff',
+			// 	diff: options.diff, 
+			// 	lang: 'text',
+			//   });
+			// console.log('diff is :');
 			}
 			catch (err) {
 				chai.assert.fail("The formatted string is different");
 			}
 		});
+
 		test("LispFormatter.format() whole file compare Test", function () {
 			try {
 				// chai.assert.equal(formatedstring.length,fmt.length);
